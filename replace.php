44a738d812ba5adbeacdeb1c18876ba1a2fd4210
armen.alaverdyan <armen.alaverdyan@i-vengo.com>
2016-01-19 16:48:40 +0300

    fixes

---
+++ replace.php
@@ -0,0 +1,33 @@
+<?php
+
+$string     = file_get_contents('_p2p.less');
+
+
+while(substr_count($string,'rem') != 0){
+    $start              = strpos($string,'rem');
+    $numberResolution   = [];
+
+    $char   = $string[$start];
+    $i      = $start;
+    $loop   = true;
+
+    while(!($char == ' ' || $char == '-')){
+        $i--;
+        $char = $string[$i];
+        $numberResolution[$i] = $char;
+    }
+    array_pop($numberResolution);
+
+    $keys = array_keys($numberResolution);
+
+    $nEnd   = reset($keys);
+    $nStart = end($keys);
+
+    $number = implode('',array_reverse($numberResolution));
+    $newNumber = round($number / 20,1);
+
+    $string = str_replace($number.'rem',$newNumber.'mer',$string);
+}
+
+header('Content-type: text/css');
+echo($string);
